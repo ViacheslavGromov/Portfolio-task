7fe55bc4ab43771eb458357c0e8e92f5eef8e0f5
Vicheslav Gromov <slavagromov123@gmail.com>
2022-01-22 23:45:40 +0300

    feat: add menu-burger(js), fix adaptive

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,12 +1,25 @@
 function score(){
-    console.log('Вёрстка валидная 10/10\n Вёрстка семантическая 20/20\n Вёрстка соответствует макету 48/48\n Требования к css 12/12\n Интерактивность, реализуемая через css 20/20');
+    console.log(' Вёрстка соответствует макету 48/48\n Ни на одном из разрешений не появляется горизонтальная полоса прокрутки 15/15\n Адаптивное меню 22/22');
 };
 
 score();
 
 (function () {
     const burgerItem = document.querySelector('.burger');
-    burgerItem.addEventListener('click', () =>{
-        console.log(123);
-    })
+    const menu = document.querySelector('.header__navigation');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.navigation__link');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click' , () => {
+        menu.classList.remove('header__nav_active');
+    });
+    if (window.innerWidth <= 768) {
+        for (let i=0; i < menuLinks.length; i +=1){
+            menuLinks[i].addEventListener('click' , () => {
+                menu.classList.remove('header__nav_active');
+            })
+        }
+    }
 }());
